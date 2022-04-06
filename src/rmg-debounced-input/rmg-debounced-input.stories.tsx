e6f31eade8113e497fabf5c4cb435c986e929ad9
Chito Wong <chi.to.wong@outlook.com>
2022-04-07 01:38:01 +0800

    Add story for non-delayed RmgDebouncedInput

--- src/rmg-debounced-input/rmg-debounced-input.stories.tsx
+++ src/rmg-debounced-input/rmg-debounced-input.stories.tsx
@@ -1,6 +1,7 @@
 import { RmgDebouncedInput } from './rmg-debounced-input';
 import { Box, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { RmgLabel } from '../rmg-label';
 
 export default {
     title: 'RmgDebouncedInput',
@@ -8,15 +9,30 @@ export default {
 };
 
 export const Basic = () => {
-    const [value, setValue] = useState('');
+    const [value0, setValue0] = useState('');
+    const [value500, setValue500] = useState('');
+
     return (
         <Box>
-            <RmgDebouncedInput
-                placeholder="Enter text in fast speed"
-                defaultValue={value}
-                onDebouncedChange={setValue}
-            />
-            <Text>Your input: {value}</Text>
+            <RmgLabel label="Delay=0ms">
+                <RmgDebouncedInput
+                    placeholder="Enter text in fast speed"
+                    defaultValue={value0}
+                    onDebouncedChange={setValue0}
+                    delay={0}
+                />
+            </RmgLabel>
+
+            <RmgLabel label="Delay=500ms">
+                <RmgDebouncedInput
+                    placeholder="Enter text in fast speed"
+                    defaultValue={value500}
+                    onDebouncedChange={setValue500}
+                />
+            </RmgLabel>
+
+            <Text>Delay=0ms: {value0}</Text>
+            <Text>Delay=500ms: {value500}</Text>
         </Box>
     );
 };
