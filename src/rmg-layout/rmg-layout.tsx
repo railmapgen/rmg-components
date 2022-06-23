06cec275f0792c58f971142c0d0558092ab66029
Chito Wong <chi.to.wong@outlook.com>
2022-06-24 00:03:34 +0800

    Make RmgLayout customisable

--- src/rmg-layout/rmg-layout.tsx
+++ src/rmg-layout/rmg-layout.tsx
@@ -1,21 +1,25 @@
 import { Flex, FlexProps, useStyleConfig } from '@chakra-ui/react';
 
 export const RmgWindow = (props: FlexProps) => {
+    const { sx, ...others } = props;
     const styles = useStyleConfig('RmgWindow');
-    return <Flex sx={styles} {...props} />;
+    return <Flex sx={{ ...styles, ...sx }} {...others} />;
 };
 
 export const RmgWindowHeader = (props: FlexProps) => {
+    const { sx, ...others } = props;
     const styles = useStyleConfig('RmgWindowHeader');
-    return <Flex sx={styles} {...props} />;
+    return <Flex sx={{ ...styles, ...sx }} {...others} />;
 };
 
 export const RmgPage = (props: FlexProps) => {
+    const { sx, ...others } = props;
     const styles = useStyleConfig('RmgPage');
-    return <Flex sx={styles} {...props} />;
+    return <Flex sx={{ ...styles, ...sx }} {...others} />;
 };
 
 export const RmgPageHeader = (props: FlexProps) => {
+    const { sx, ...others } = props;
     const styles = useStyleConfig('RmgPageHeader');
-    return <Flex sx={styles} {...props} />;
+    return <Flex sx={{ ...styles, ...sx }} {...others} />;
 };
