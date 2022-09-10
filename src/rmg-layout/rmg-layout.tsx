f232f591751014aeb9232813222b5e83cdcc2fa4
Chito Wong <chi.to.wong@outlook.com>
2022-09-10 16:36:03 +0800

    Make RmgLayout components class extendable

--- src/rmg-layout/rmg-layout.tsx
+++ src/rmg-layout/rmg-layout.tsx
@@ -1,25 +1,25 @@
 import { Flex, FlexProps, useStyleConfig } from '@chakra-ui/react';
 
 export const RmgWindow = (props: FlexProps) => {
-    const { sx, ...others } = props;
+    const { sx, className, ...others } = props;
     const styles = useStyleConfig('RmgWindow');
-    return <Flex className="rmg-window" sx={{ ...styles, ...sx }} {...others} />;
+    return <Flex className={`rmg-window ${className ?? ''}`} sx={{ ...styles, ...sx }} {...others} />;
 };
 
 export const RmgWindowHeader = (props: FlexProps) => {
-    const { sx, ...others } = props;
+    const { sx, className, ...others } = props;
     const styles = useStyleConfig('RmgWindowHeader');
-    return <Flex className="rmg-window__header" sx={{ ...styles, ...sx }} {...others} />;
+    return <Flex className={`rmg-window__header ${className ?? ''}`} sx={{ ...styles, ...sx }} {...others} />;
 };
 
 export const RmgPage = (props: FlexProps) => {
-    const { sx, ...others } = props;
+    const { sx, className, ...others } = props;
     const styles = useStyleConfig('RmgPage');
-    return <Flex className="rmg-page" sx={{ ...styles, ...sx }} {...others} />;
+    return <Flex className={`rmg-page ${className ?? ''}`} sx={{ ...styles, ...sx }} {...others} />;
 };
 
 export const RmgPageHeader = (props: FlexProps) => {
-    const { sx, ...others } = props;
+    const { sx, className, ...others } = props;
     const styles = useStyleConfig('RmgPageHeader');
-    return <Flex className="rmg-page__header" sx={{ ...styles, ...sx }} {...others} />;
+    return <Flex className={`rmg-page__header ${className ?? ''}`} sx={{ ...styles, ...sx }} {...others} />;
 };
