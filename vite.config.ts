e40f4609432835f4ef0a2c6f6cde4221267cf4e8
Chito Wong <chi.to.wong@outlook.com>
2023-08-17 20:58:28 +0800

    #NOISSUE Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config
 export default defineConfig({
     base: '/rmg-components/',
-    plugins: [
-        react(),
-        splitVendorChunkPlugin(),
-    ],
+    plugins: [react(), splitVendorChunkPlugin()],
     test: {
         globals: true,
         environment: 'jsdom',
         setupFiles: './src/setupTests.ts',
-        deps: {
-            fallbackCJS: true,
+        server: {
+            deps: {
+                fallbackCJS: true,
+            },
         },
         watch: false,
     },
