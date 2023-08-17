b93a3b990a5471b257cc5bd9e6efdc7500017402
Chito Wong <chi.to.wong@outlook.com>
2023-08-17 21:59:32 +0800

    #1 Remove FocusLock from RmgAppClip

--- src/rmg-app-clip/rmg-app-clip.tsx
+++ src/rmg-app-clip/rmg-app-clip.tsx
@@ -1,4 +1,4 @@
-import { BoxProps, chakra, FocusLock, ModalProps, Portal, useOutsideClick, useStyleConfig } from '@chakra-ui/react';
+import { BoxProps, chakra, ModalProps, Portal, useOutsideClick, useStyleConfig } from '@chakra-ui/react';
 import { useRef } from 'react';
 
 export interface RmgAppClipProps extends BoxProps {
@@ -20,11 +20,9 @@ export const RmgAppClip = (props: RmgAppClipProps) => {
             <chakra.div className={isOpen ? 'show-app-clip' : ''} sx={styles} {...others}>
                 <div className="rmg-app-clip__overlay" />
                 <div className="rmg-app-clip__container">
-                    <FocusLock>
-                        <chakra.section ref={contentRef} role="dialog" className="rmg-app-clip__content" sx={sx}>
-                            {children}
-                        </chakra.section>
-                    </FocusLock>
+                    <chakra.section ref={contentRef} role="dialog" className="rmg-app-clip__content" sx={sx}>
+                        {children}
+                    </chakra.section>
                 </div>
             </chakra.div>
         </Portal>
